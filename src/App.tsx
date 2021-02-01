e0393a11d597d2c42ee11c0dd477803feed0bcdf
Benjamin Gudehus <hastebrot@gmail.com>
2021-02-01 11:25:02 +0100

    (chore) Add tailwindcss dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="font-sans bg-green-100 text-green-900 h-screen p-4">
+      <p>
+        <button
+          className="rounded border border-green-700 px-1"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is: {count}
+        </button>
+      </p>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
+};
